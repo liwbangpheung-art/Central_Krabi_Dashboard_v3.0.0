5ff8118ca50de9a165caf21dd938b1acaba0c71d
liwbangpheung-art <liwbangpheung@gmail.com>
2026-07-09 20:36:39 +0700

    Delete backend directory

--- backend/src/middleware/request-context.js
+++
@@ -1,8 +0,0 @@
-import { randomUUID } from "node:crypto";
-
-export function requestContext(req, res, next) {
-  const requestId = req.header("x-request-id") || randomUUID();
-  req.requestId = requestId;
-  res.setHeader("x-request-id", requestId);
-  next();
-}
